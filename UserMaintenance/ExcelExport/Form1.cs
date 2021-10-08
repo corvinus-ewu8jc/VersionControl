e2ac7c76cc49f8393d226fd3508da1f917ae1c28
Júlia Südi <julia.sudi@stud.uni-corvinus.hu>
2021-10-08 12:50:44 +0200

    Create Excel

--- UserMaintenance/ExcelExport/Form1.cs
+++ UserMaintenance/ExcelExport/Form1.cs
@@ -9,7 +9,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Excel= Microsoft.Office.Interop.Excel;
-using System.Reflection
+using System.Reflection;
 
 
 namespace ExcelExport
@@ -19,16 +19,50 @@ namespace ExcelExport
 
         RealEstateEntities context = new RealEstateEntities();
         List<Flat> lakasok;
+        Excel.Application xlApp;
+        Excel.Workbook xlWb;
+        Excel.Worksheet xlSheet;
+
             
         public Form1()
         {
             InitializeComponent();
             LoadData();
             dataGridView1.DataSource = lakasok;
+            CreateExcel();
         }
         public void LoadData()
         {
             lakasok = context.Flats.ToList();
+        }
+
+        public void CreateExcel()
+        {
+            try
+            {
+                xlApp = new Excel.Application();
+                xlWb = xlApp.Workbooks.Add(Missing.Value);
+                xlSheet = xlWb.ActiveSheet;
+
+                CreateTable();
+
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+            }
+            catch (Exception ex)
+            {
+                string hiba = string.Format("Error:{0\nLine: {1}}", ex.Message, ex.Source);
+                MessageBox.Show(hiba, "Error");
+
+                xlWb.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWb = null;
+                xlApp = null;
+            }
+        }
+
+        private void CreateTable()
+        { 
         }
 
         private void Form1_Load(object sender, EventArgs e)
