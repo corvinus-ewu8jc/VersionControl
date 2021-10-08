ff6317b76b16b54f4ff7fdd73706a7859d5a3cc2
Júlia Südi <julia.sudi@stud.uni-corvinus.hu>
2021-10-08 13:15:27 +0200

    Excel feltöltés

--- UserMaintenance/ExcelExport/Form1.cs
+++ UserMaintenance/ExcelExport/Form1.cs
@@ -62,9 +62,55 @@ namespace ExcelExport
         }
 
         private void CreateTable()
-        { 
-        }
+        {
+            string[] headers = new string[]
+            {
+                "Kód",
+                "Eladó",
+                "Oldal",
+                "Kerület",
+                "Lift",
+                "Szobák száma",
+                "Alapterület (m2)",
+                "Ár (mFt)",
+                "Négyzetméter ár (Ft/m2)"
+            };
 
+            for (int i = 0; i < headers.Length; i++)
+            {
+                xlSheet.Cells[1, i+1] = headers[i];
+            }
+
+            object[,] values= new object[lakasok.Count, headers.Length];
+
+            int counter = 0;
+            foreach (var lakas in lakasok)
+            {
+                values[counter, 0] = lakas.Code;
+                counter++;
+            }
+            var range= xlSheet.get_Range(
+                GetCell(0, 0),
+                GetCell(values.GetLength(0), values.GetLength(1))
+                );
+            range.Value2 = values;
+        }
+        private string GetCell(int x, int y)
+        {
+            string ExcelCoordinate = "";
+            int dividend = y;
+            int modulo;
+
+            while (dividend > 0)
+            {
+                modulo = (dividend - 1) % 26;
+                ExcelCoordinate = Convert.ToChar(65 + modulo).ToString() + ExcelCoordinate;
+                dividend = (int)((dividend - modulo) / 26);
+            }
+            ExcelCoordinate += x.ToString();
+
+            return ExcelCoordinate;
+        }
         private void Form1_Load(object sender, EventArgs e)
         {
             // TODO: This line of code loads data into the 'realEstateDataSet.Flat' table. You can move, or remove it, as needed.
