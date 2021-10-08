0cc23a1e1df5f7083d78e8ac872f6dcbaa1a9cb2
Júlia Südi <julia.sudi@stud.uni-corvinus.hu>
2021-10-08 12:26:40 +0200

    Excel NameSpace

--- UserMaintenance/ExcelExport/Form1.cs
+++ UserMaintenance/ExcelExport/Form1.cs
@@ -3,18 +3,39 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Excel= Microsoft.Office.Interop.Excel;
+using System.Reflection
+
 
 namespace ExcelExport
 {
     public partial class Form1 : Form
     {
+
+        RealEstateEntities context = new RealEstateEntities();
+        List<Flat> lakasok;
+            
         public Form1()
         {
             InitializeComponent();
+            LoadData();
+            dataGridView1.DataSource = lakasok;
+        }
+        public void LoadData()
+        {
+            lakasok = context.Flats.ToList();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'realEstateDataSet.Flat' table. You can move, or remove it, as needed.
+            this.flatTableAdapter.Fill(this.realEstateDataSet.Flat);
+
         }
     }
 }
