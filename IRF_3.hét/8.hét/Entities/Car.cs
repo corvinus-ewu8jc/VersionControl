00479e2b371ea5626e73ecb8566540c906702af4
schie <schie@ACER-Z8>
2021-11-12 18:59:39 +0100

    Autó létrehozása

---
+++ IRF_3.hét/8.hét/Entities/Car.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace _8.hét.Entities
+{
+    public class Car : Abstractions.Toy
+    {
+        protected override void DrawImage(Graphics g)
+        {
+            Image imageFile = Image.FromFile("Images/car.png");
+            g.DrawImage(imageFile, new Rectangle(0, 0, Width, Height));
+        }
+    }
+}
