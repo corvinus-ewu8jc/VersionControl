379fabc4298b512e43c70665d808cb316f26c1ad
Júlia Südi <julia.sudi@stud.uni-corvinus.hu>
2021-11-30 18:14:29 +0100

    Szimuláció

--- IRF_3.hét/IRF_9.hét/Form1.cs
+++ IRF_3.hét/IRF_9.hét/Form1.cs
@@ -20,6 +20,28 @@ namespace IRF_9.hét
             Population = GetPopulation(@"C:\Temp\nép.csv");
             BirthProbabilities = GetBirthProbabilities(@"C:\Temp\születés.csv");
             DeathProbabilities = GetDeathProbabilities(@"C:\Temp\halál.csv");
+
+
+            //Szimuláció
+
+            // Végigmegyünk a vizsgált éveken
+            for (int year = 2005; year <= 2024; year++)
+            {
+                // Végigmegyünk az összes személyen
+                for (int i = 0; i < Population.Count; i++)
+                {
+                    // Ide jön a szimulációs lépés
+                }
+
+                int nbrOfMales = (from x in Population
+                                  where x.Gender == Gender.Male && x.IsAlive
+                                  select x).Count();
+                int nbrOfFemales = (from x in Population
+                                    where x.Gender == Gender.Female && x.IsAlive
+                                    select x).Count();
+                Console.WriteLine(
+                    string.Format("Év:{0} Fiúk:{1} Lányok:{2}", year, nbrOfMales, nbrOfFemales));
+            }
         }
 
         List<Person> Population = new List<Person>();
@@ -89,5 +111,9 @@ namespace IRF_9.hét
 
             return deathProbabilities;
         }
+
+        Random rng = new Random(1234);
+
+            
     }
 }
